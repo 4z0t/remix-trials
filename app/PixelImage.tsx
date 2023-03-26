2efd16fdb174d4a4c54dbc38fa93615f36f86d05
4z0t <gastec139@gmail.com>
2023-03-26 18:44:11 +0300

    cleanup

--- app/PixelImage.tsx
+++ app/PixelImage.tsx
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
-import { LinksFunction } from "@remix-run/react/dist/routeModules";
-import "./PixelImage.css";
-
 export { PixelImage };
-// export const links: LinksFunction = () => [
-//   { rel: "stylesheet", href: stylePixelImage },
-// ];
-
 class PixelImage extends Component<
   React.ImgHTMLAttributes<HTMLImageElement>,
   React.CSSProperties
